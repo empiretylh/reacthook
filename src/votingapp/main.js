465575c3848c29803ec10783ce2155a6c566ed40
Thura Lin Htut <thuralinhtut3@gmail.com>
2022-12-10 05:14:03 +0000

    commit

--- src/votingapp/main.js
+++ src/votingapp/main.js
@@ -1,21 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { baseURL } from "./data/data";
 
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 axios.defaults.baseURL = baseURL;
 import Home from "./screens/home";
-
+import Login from './screens/login';
+import { useEffect } from "react";
+import services from "./data/services";
 
 const VotingMain = () => {
-    
-    const [token,setToken] = useState()
+  const [token, setToken] = useState(null);
 
+  useEffect(() => {
+    const current_token = services.getCurrentUserToken();
+    setToken(current_token);
+  }, [token]);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {token === null ? (
+          <Route path="/" element={<Login />} />
+        ) : (
+          <Route path="/" element={<Home />} />
+        )}
       </Routes>
     </Router>
   );
